b9e3398ad4cb787a2a37a2c20580c11887268188
iankimble <ian.kimble571@gmail.com>
2019-07-05 11:06:33 -0400

    pushing react app

---
+++ src/components/AboutApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutApp extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>About</h1>
+        <button><a href="../public/index.html">click me</a></button>
+      </div>  
+    )
+  }
+}
+
+export default AboutApp;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+Header = ()=>(
+
+<div>
+
+    <h1> Ello Again</h1>
+
+</div>
+
+);
+
+export default Header;
